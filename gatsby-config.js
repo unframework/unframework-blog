a803dfe247edc333ffe2b0de715ac21e32bdbc51
Nick Matantsev <nick.matantsev@gmail.com>
2020-05-02 16:33:38 -0400

    Install TypeScript and rename files

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,9 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-typescript`
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
