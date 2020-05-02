f913089972c5e69f868f551002372c6d0f2256d4
Nick Matantsev <nick.matantsev@gmail.com>
2020-05-02 18:49:43 -0400

    Replace typography theme with Wikipedia, add typings

---
+++ src/utils/typography-theme-wikipedia.d.ts
@@ -0,0 +1,6 @@
+declare module 'typography-theme-wikipedia' {
+  import { TypographyOptions } from 'typography';
+
+  const options: TypographyOptions;
+  export default options;
+}
--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,7 +1,7 @@
 import Typography from 'typography';
-import Wordpress2016 from 'typography-theme-wordpress-2016';
+import ThemeWikipedia from 'typography-theme-wikipedia';
 
-Wordpress2016.overrideThemeStyles = () => {
+ThemeWikipedia.overrideThemeStyles = () => {
   return {
     'a.gatsby-resp-image-link': {
       boxShadow: `none`
@@ -9,9 +9,9 @@ Wordpress2016.overrideThemeStyles = () => {
   };
 };
 
-delete Wordpress2016.googleFonts;
+delete ThemeWikipedia.googleFonts;
 
-const typography = new Typography(Wordpress2016);
+const typography = new Typography(ThemeWikipedia);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
